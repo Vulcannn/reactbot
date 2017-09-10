b9b4d8bf293b5b577b2c8a19e788cc05ab68270f
Ewan <vulcanv6@hotmail.com>
2017-09-10 20:01:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,20 @@
 import discord
 from discord.ext import commands #run in cmd: pip install discord.py
 
-token = 'botoken'
+token = 'botoken' #enter bot token in here
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('---------------------------------------------')
+    print('Bot has started!')
+    print('Bot Logged in as:', client.user.name)
+    print('Bot User ID:', client.user.id)
+    print('---------------------------------------------')
+    
+
+    
+    
+    
+    
+client.start(token)
